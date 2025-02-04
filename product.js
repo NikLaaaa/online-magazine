ac954833e9733200ec5dabb97af0e4aababddf77
sergey123423213 <grinya.0895@gmail.com>
2025-02-04 11:13:59 +0200

    refactoring js from Sergey

--- product.js
+++ product.js
@@ -1,40 +1,43 @@
-const nameOfProducts = ['Dunk','Air Vapormax','Blazer','Jordan 1 Retro Mid/High','Air Force','Jordan 1 Retro Low','Jordan 4 Retro']
+// arrays
+const nameOfProducts = ['Dunk', 'Air Vapormax', 'Blazer', 'Jordan 1 Retro Mid/High', 'Air Force', 'Jordan 1 Retro Low', 'Jordan 4 Retro']
+const pricesProduct = ['3000 грн', '4000 грн', '3900 грн', '3500грн', '4200 грн', '4400 грн', '3700 грн']
+const sizesProduct = ["36", "37", "38", "39", "40", "41", "42", "43", "44", "45"]
+// variable
 const CategoryOfShoes = document.querySelector(".CategoryOfShoes")
-const pricesProduct = ['3000 грн','4000 грн','3900 грн','3500грн','4200 грн','4400 грн','3700 грн']
-const categoryModal = document.querySelector(".categoryModal")
-const categoryModalContentClose = document.querySelector(".categoryModal-content-close")
-nameOfProducts.forEach(function(element,index){
-    CategoryOfShoes.innerHTML+=`
-                <div class="CategoryOfShoes-item">
-                    <img src="/src/img/product-${index+1}.png" alt="" class="CategoryOfShoes-item-product">
-                    <p class="CategoryOfShoes-item-name">${element}</p>
-                <p class="CategoryOfShoes-item-price">${pricesProduct[index]}</p>
-                </div>
+
+nameOfProducts.forEach(function (element, index) {
+    CategoryOfShoes.innerHTML += `
+        <div class="CategoryOfShoes-item">
+            <img src="/src/img/product-${index + 1}.png" alt="" class="CategoryOfShoes-item-product">
+            <p class="CategoryOfShoes-item-name">${element}</p>
+            <p class="CategoryOfShoes-item-price">${pricesProduct[index]}</p>
+        </div>
     `
 })
-
-const CategoryOfShoesItemBuy = document.querySelectorAll(".CategoryOfShoes-item")
-const categoryModalContentProduct = document.querySelector(".categoryModal-content-product")
-const categoryModalContentName = document.querySelector(".categoryModal-content-name")
-const categoryModalContentPrice = document.querySelector(".categoryModal-content-price")
-
-CategoryOfShoesItemBuy.forEach(function(element,index){
-    element.addEventListener('click',()=>{
-        categoryModalContentName.textContent = element.children[1].textContent
-        categoryModalContentPrice.textContent = element.lastElementChild.textContent
-        categoryModalContentProduct.setAttribute('src',element.firstElementChild.getAttribute('src'))
-        categoryModal.style.display = 'flex'
+// All Variables
+const categoryVariables = {
+    categoryModal: document.querySelector(".categoryModal"),
+    categoryModalContentClose: document.querySelector(".categoryModal-content-close"),
+    CategoryOfShoesItemBuy: document.querySelectorAll(".CategoryOfShoes-item"),
+    categoryModalContentProduct: document.querySelector(".categoryModal-content-product"),
+    categoryModalContentName: document.querySelector(".categoryModal-content-name"),
+    categoryModalContentPrice: document.querySelector(".categoryModal-content-price"),
+    shoeSize:document.getElementById("shoe-size")
+}
+categoryVariables.CategoryOfShoesItemBuy.forEach(function (element, index) {
+    element.addEventListener('click', () => {
+        categoryVariables.categoryModalContentName.textContent = element.children[1].textContent
+        categoryVariables.categoryModalContentPrice.textContent = element.lastElementChild.textContent
+        categoryVariables.categoryModalContentProduct.setAttribute('src', element.firstElementChild.getAttribute('src'))
+        categoryVariables.categoryModal.style.display = 'flex'
     })
 })
-categoryModalContentClose.addEventListener("click",()=>{
-    categoryModal.style.display = "none"
+categoryVariables.categoryModalContentClose.addEventListener("click", () => {
+    categoryVariables.categoryModal.style.display = "none"
 })
-
-const sizesProduct = ["36","37","38","39","40","41","42","43","44","45"]
-const shoeSize = document.getElementById("shoe-size")
-sizesProduct.forEach(function(element,index){
+sizesProduct.forEach(function (element, index) {
     const option = document.createElement("option")
-    option.setAttribute("value",element)
+    option.setAttribute("value", element)
     option.textContent = element
-    shoeSize.append(option)
+    categoryVariables.shoeSize.append(option)
 })
